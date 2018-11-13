4f4e0133be9ed870f39325a560ed0e641674e730
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2018-11-13 00:48:31 -0500

    add methods parents, all_in reformat codes

--- enumeration.py
+++ enumeration.py
@@ -21,13 +21,13 @@ def Enumeration_Ask(X='', e=[], bn=[], data={}):
         tmp_bn = bn.copy()
         tmp_e.append(x)
         tmp_bn = [x] + tmp_bn
-        print([tmp_bn, tmp_e])
+        # print(tmp_bn, tmp_e)
         dis = Enumeration_ALL(tmp_bn, tmp_e, data)
         distribution.append(dis)
     return Normalize(distribution)
 
 
-def match(s=set(), e=[]):
+def all_in(s=set(), e=[]):
     if (s.__len__() == 0): return True
     for i in s:
         if i not in e:
@@ -36,44 +36,42 @@ def match(s=set(), e=[]):
     return True
 
 
+def parents(variable, data):
+    ps = []
+    for key in data.keys():
+        if key.fore == variable:
+            ps.append(key)
+    return ps
+
+
 def Enumeration_ALL(bn, e, data):
     if bn.__len__() == 0:
         return 1
 
     if bn[0] in e:
-        parents = []
-        for key in data.keys():
-            if key.fore == bn[0]:
-                parents.append(key)
-        for parent in parents:
-            given = parent.given
-            if match(given, e):
-                rest = bn[1:]
-                return data[parent] * Enumeration_ALL(rest, e, data)
+        for parent in parents(bn[0], data):
+            if all_in(parent.given, e):
+                rest_bn = bn[1:]
+                return data[parent] * Enumeration_ALL(rest_bn, e, data)
 
     else:
         variable_true = bn[0].lower()
         variable_false = '!' + bn[0].lower()
-        e_true = e.copy()
-        e_true.append(variable_true)
-        e_false = e.copy()
-        e_false.append(variable_false)
-        parents = []
         given = set()
-        rest = []
-        for key in data.keys():
-            if key.fore == variable_true:
-                parents.append(key)
-        for parent in parents:
-            tmp_given = parent.given
-            if match(tmp_given, e):
-                rest = bn[1:]
-                given = tmp_given
-        p_true = Probability(variable_true, given)
-        p_false = Probability(variable_false, given)
-        return data[p_true] * Enumeration_ALL(rest, e_true, data) + data[p_false] * Enumeration_ALL(rest, e_false, data)
+        rest_bn = []
+        # variable_true and variable_false both represents the same node
+        for parent in parents(variable_true, data):
+            if all_in(parent.given, e):
+                rest_bn = bn[1:]
+                given = parent.given
+                break
+        e_true = e.copy()
+        e_false = e.copy()
+        e_true.append(variable_true)
+        e_false.append(variable_false)
+        return data[Probability(variable_true, given)] * Enumeration_ALL(rest_bn, e_true, data) + \
+            data[Probability(variable_false, given)] * Enumeration_ALL(rest_bn, e_false, data)
 
 
 data = test.readdata('aima-alarm.xml')
-l = Enumeration_Ask('B', ['j', 'm'], ['E', 'A', 'j', 'm'], data)
-print(l)
+print(Enumeration_Ask('B', ['j', 'm'], ['E', 'A', 'j', 'm'], data))
