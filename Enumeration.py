ea6e6334c1ee9132f2ae29f560052178c7f6aa18
zhangyu <18301644230@163.com>
2018-11-14 13:23:44 -0500

    rej finish

--- Enumeration.py
+++ Enumeration.py
@@ -1,12 +1,6 @@
 import copy
 from test import *
 
-
-class Node:
-    pass
-
-
-# evidence:set()
 def in_e(a, evidence):
     a = a.lower()
     for e in evidence:
@@ -16,7 +10,6 @@ def in_e(a, evidence):
             return 3
     return 1
 
-
 def get_parents(var, bn):
     result = set()
     for key in bn.keys():
@@ -24,7 +17,6 @@ def get_parents(var, bn):
             for given in key.given:
                 result.add(given)
     return result
-
 
 def tp_sort(vars, bn):
     result = list()
@@ -40,13 +32,10 @@ def tp_sort(vars, bn):
                     break
             if flg == 0:
                 result.append(var)
-            # else:
-            #     result.append(var)
         else:
             result.append(var)
-    result.reverse()
+    # result.reverse()
     return result
-
 
 def enumeration_ask(X, e, bn):
     variables = set()
@@ -57,31 +46,10 @@ def enumeration_ask(X, e, bn):
     e_not_x = copy.deepcopy(e)
     e_not_x.add('!' + X.lower())
     result = dict()
-    bnx = copy.deepcopy(bn)
-    bnnotx = copy.deepcopy(bn)
-    for sentence, v in bn.items():
-        flg1 = 0
-        flg2 = 0
-        for given in sentence.given:
-            if in_e(given, e_x) == 3:
-                flg1 = 1
-            if in_e(given, e_not_x) == 3:
-                flg2 = 2
-        if flg1 == 1:
-            bnx.pop(sentence)
-        if flg2 == 2:
-            bnnotx.pop(sentence)
-    a = ["E", "B", "A", "J", "M"]
-    b = ["E", "B", "A", "J", "M"]
-    a.reverse()
-    b.reverse()
-    print(tp_sort(list(variables), bnx))
-    result[X.lower()] = enumerate_all(copy.deepcopy(tp_sort(list(variables), bnx)), e_x, bnx, '')
-    print('___________________')
-    result['!' + X.lower()] = enumerate_all(copy.deepcopy(tp_sort(list(variables), bnnotx)), e_not_x, bnnotx, '')
+    result[X.lower()] = enumerate_all(copy.deepcopy(tp_sort(list(variables), bn)), e_x, bn)
+    result['!' + X.lower()] = enumerate_all(copy.deepcopy(tp_sort(list(variables), bn)), e_not_x, bn)
     normalize(result)
     return result
-
 
 def normalize(result):
     total = 0
@@ -90,116 +58,39 @@ def normalize(result):
     for k in result.keys():
         result[k] = result[k] / total
 
+def check(keys, e):
+    flg = True
+    for given in keys.given:
+        if in_e(given, e) == 3:
+            flg = False
+    return flg
 
-def enumerate_all(variables, e, data, str):
-    # print(e)
-
+def enumerate_all(variables, e, data):
     if len(variables) < 1:
-        print(str)
         return 1.0
     y = variables.pop()
+    sum_p = 0
     if in_e(y, e) == 1:
-        sum_p = 0
         e_y = copy.deepcopy(e)
         e_y.add(y.lower())
         e_not_y = copy.deepcopy(e)
         e_not_y.add('!' + y.lower())
         for keys, val in data.items():
-            if keys.fore == y.lower():
-                # print(keys.fore, y.lower(), e)
-                flg = True
-                for given in keys.given:
-                    print(given, e)
-                    if in_e(given, e) == 3:
-                        flg = False
-                if flg:
-                    sum_p += (float(val) * enumerate_all(copy.deepcopy(variables), e_y, data,
-                                                         str + keys.tostring() + val))
-                else:
-                    print('==========')
-            elif ((keys.fore == '!' + y.lower()) or (keys.fore == y.lower().strip('!'))) and (keys.fore != y.lower()):
-                # else:
-                # print(keys.fore, y.lower(), e)
-                flg = True
-                for given in keys.given:
-                    print(given, e)
-                    if in_e(given, e) == 3:
-                        flg = False
-                if flg:
-                    sum_p += (float(val) * enumerate_all(copy.deepcopy(variables), e_not_y, data,
-                                                         str + keys.tostring() + val))
-                else:
-                    print('==========')
-        return sum_p
+            if keys.fore == y.lower() and check(keys, e):
+                    sum_p += (float(val) * enumerate_all(copy.deepcopy(variables), e_y, data))
+            elif ((keys.fore == '!' + y.lower()) or (keys.fore == y.lower().strip('!'))) and (keys.fore != y.lower()) and check(keys, e):
+                    sum_p += (float(val) * enumerate_all(copy.deepcopy(variables), e_not_y, data))
     else:
         if in_e(y, e) == 2:
-            sum_p = 0
             for keys, val in data.items():
-                if keys.fore == y.lower():
-                    # print(keys.fore, y.lower(), e,2)
-                    flg = True
-                    for given in keys.given:
-                        print(given, e)
-                        if in_e(given, e) == 3:
-                            flg = False
-                    if flg:
-                        sum_p += (float(val) * enumerate_all(copy.deepcopy(variables), copy.deepcopy(e), data,
-                                                             str + keys.tostring() + val))
-                    else:
-                        print('==========')
-            return sum_p
+                if keys.fore == y.lower() and check(keys, e):
+                        sum_p += (float(val) * enumerate_all(variables, copy.deepcopy(e), data))
         else:
-            sum_p = 0
             for keys, val in data.items():
-                if ((keys.fore == '!' + y.lower()) or (keys.fore == y.lower().strip('!'))) and (keys.fore != y.lower()):
-                    # print(keys.fore,y.lower(),e,3)
-                    flg = True
-                    for given in keys.given:
-                        print(given, e)
-                        if in_e(given, e) == 3:
-                            flg = False
-                    if flg:
-                        sum_p += (float(val) * enumerate_all(copy.deepcopy(variables), copy.deepcopy(e), data,
-                                                             str + keys.tostring() + val))
-                    else:
-                        print('==========')
-            return sum_p
-
-    #
-    #
-    #
-    #
-    # if y in list(e.keys()):
-    #     if y.lower() in e:
-    #         sum_p = 0
-    #         for keys, val in data.items():
-    #             if keys.fore == y.lower():
-    #                 sum_p += float(val) * enumerate_all(variables, e, data)
-    #                 print(keys.tostring(), 'e')
-    #     else:
-    #         sum_p = 0
-    #         for keys, val in data.items():
-    #             if keys.fore == '!' + y.lower():
-    #                 sum_p += float(val) * enumerate_all(variables, e, data)
-    #                 print(keys.tostring(), '!e')
-    #     return sum_p
-    # else:
-    #     sum_p = 0
-    #     noty = '!' + y.lower()
-    #     e_noty = copy.deepcopy(e)
-    #     e_y = copy.deepcopy(e)
-    #     e_y[y] = y.lower()
-    #     e_noty[y] = noty
-    #     for keys, val in data.items():
-    #         if keys.fore == y.lower():
-    #             sum_p += float(val) * enumerate_all(variables, e_y, data)
-    #             print(keys.tostring(), 2)
-    #         elif keys.fore == noty:
-    #             sum_p += float(val) * enumerate_all(variables, e_noty, data)
-    #             print(keys.tostring(), 3)
-    #     return sum_p
+                if ((keys.fore == '!' + y.lower()) or (keys.fore == y.lower().strip('!'))) and (keys.fore != y.lower()) and check(keys, e):
+                        sum_p += (float(val) * enumerate_all(variables, copy.deepcopy(e), data))
+    return sum_p
 
 
-data = readdata('dog-problem.xml')
-print(enumeration_ask("BOWEL-PROBLEM", {"dog-out", 'hear-bark'}, data))
-
+data = readdata('aima-alarm.xml')
+# print(enumeration_ask("B", {"j", 'm'}, data))
