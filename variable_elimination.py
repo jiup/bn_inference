8b51c76b34a70229bb367783a4a607b021240a07
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2018-11-28 01:35:07 -0500

    updated ve algorithm, but buggy code

--- variable_elimination.py
+++ variable_elimination.py
@@ -1,6 +1,5 @@
-import test
+import xmlparser
 from exact_inference import *
-from test import Probability
 
 
 def normalize(dist):
@@ -30,36 +29,47 @@ def valid_query(data, var):
     return False
 
 
+# B     E
+#    A
+# j     m
+#
+# A -> B -> E -> j -> m
 def elimination_ask(query, variables, evidences, data, parents):
     factors = []
     rest_variables = variables
     while len(rest_variables) > 0:
         var = rest_variables[0]
-        print('->', rest_variables)
+        print('->', var, rest_variables)
         rest_variables, factor = make_factor(var, rest_variables, evidences, data, parents)
-        if len(factor[0]) > 0:
+        if var != query and var.lower() not in evidences and '!' + var.lower() not in evidences:
             factors.append(factor)
-        # if var != query and var.lower() not in evidences:
-        #     factors = sum_out(var, factors)
+            print('before sum_out', factors)
+            factors = sum_out(var, factors, rest_variables)
+            print('after  sum_out', factors)
+    query_pos = query.lower()
+    query_neg = '!' + query.lower()
     # print(factors)
-    return
-    # return normalize(pointwise_product(factors, variables))
+    return normalize([factors[0][1][frozenset(set(query_pos))], factors[0][1][frozenset({query_neg})]])
 
 
 def make_factor(var, variables, evidences, data, parents):
     related = related_vars(var, variables + evidences, data)
-    rest_variables = [i for i in variables if i not in related]
+    # rest_variables = [i for i in variables if i not in related]
+    rest_variables = variables[1:]
     tag = set([i for i in related if i.isupper()])
     for r in related:
         tag |= {i for i in given_vars(r, variables, data) if i.isupper()}
     tag.remove(var)
     cp_table = {}
     cpt(var, tag, related, variables, evidences, data, parents, cp_table)
-    print('𝜓(' + ', '.join(tag) + ')', '=', cp_table)
-    return rest_variables, (tag, cp_table)
+    # print('𝜓(' + ', '.join(tag) + ')', '=', cp_table)
+    return rest_variables, ({var} | tag, cp_table)
 
 
-def cpt(var, tag, related_fores, variables, evidences,  data, parents, result):
+# B     E
+#    A
+# j     m
+def cpt(var, tag, related_fores, variables, evidences, data, parents, result):
     for t in tag:
         if t.isupper():
             tmp_tag = tag.copy()
@@ -68,44 +78,45 @@ def cpt(var, tag, related_fores, variables, evidences,  data, parents, result):
             cpt(var, tmp_tag | {'!' + t.lower()}, related_fores, variables, evidences, data, parents, result)
             return
     if var.isupper():
-        obs_var = [var.lower(), '!' + var.lower()]
+        cpt(var.lower(), tag, related_fores, variables, evidences, data, parents, result)
+        cpt('!' + var.lower(), tag, related_fores, variables, evidences, data, parents, result)
     else:
-        obs_var = [var]
-    prob = 0
-    # print(var, tag, related_fores, variables, evidences)
-    for v in obs_var:
         p = 1
         for fore in related_fores:
-            if fore == var:
-                fore = v
-            p *= test.Prob(fore, parents, data, [v] + list(tag) + evidences)
-            #print([v] + list(tag) + evidences, fore, '|', parents[fore], '=', test.Prob(fore, parents, data, [v] + list(tag) + evidences))
-        # print('multiplied =', p)
-        prob += p
-    result[frozenset(tag)] = prob
+            if fore.lower() == var.strip('!'):
+                fore = var
+            elif fore.isupper():
+                new_related = set(related_fores)
+                new_related.remove(fore)
+                cpt(var, tag, list(new_related) + [fore.lower()], variables, evidences, data, parents, result)
+                cpt(var, tag, list(new_related) + ['!' + fore.lower()], variables, evidences, data, parents, result)
+                return
+            # print(fore, '|', [var] + list(tag) + evidences, '=', xmlparser.Prob(fore, parents, data, [var] + list(tag) + evidences))
+            p *= xmlparser.Prob(fore, parents, data, [var] + list(tag) + evidences)
+        result[frozenset({var} | tag)] = p
+        # print('result[', frozenset({var} | tag), '] = ', p)
     return
 
 
-def sum_out(var, factors):
+def sum_out(var, factors, variables):
     result = []
-    var_true = var.lower()
-    var_false = '!' + var.lower()
-    print(factors)
+    focused_factors = []
     for factor in factors:
         tag, cp_table = factor
         if var in tag:
-            print(var, tag)
-            new_tag = tag
-            new_tag.remove(var)
-            events = []
-            all_events(list(new_tag), events)
-            table = {}
-            for e in events:
-                print(set(e) | {var_true}, cp_table)
-                table[frozenset(e)] = cp_table[frozenset(set(e) | {var_true})] + cp_table[frozenset(set(e) | {var_false})]
-            result.append((new_tag, table))
+            focused_factors.append(factor)
         else:
             result.append(factor)
+    for factor in pointwise_product(focused_factors, variables):
+        tag, cp_table = factor
+        new_tag = tag
+        new_tag.remove(var)
+        events = []
+        all_events(list(new_tag), events)
+        table = {}
+        for e in events:
+            table[frozenset(e)] = cp_table[frozenset(set(e) | {var.lower()})] + cp_table[frozenset(set(e) | {'!' + var.lower()})]
+        result.append((new_tag, table))
     return result
 
 
@@ -121,7 +132,23 @@ def all_events(variables, result):
 
 
 def pointwise_product(factors, variables):
-    pass
+    table = {}
+    vars = set()
+    for factor in factors:
+        vars |= factor[0]
+    events = []
+    all_events(list(vars), events)
+    for event in events:
+        p = 1
+        for factor in factors:
+            tag, cp_table = factor
+            es = set()
+            for e in event:
+                if e.strip('!').upper() in tag:
+                    es.add(e)
+            p *= cp_table[frozenset(es)]
+        table[frozenset(set(event))] = p
+    return [(vars, table)]
 
 
 def get_variables(query, evidences, data):
@@ -161,15 +188,8 @@ def given_vars(variable, variables, data):
     return given
 
 
-_data, _parents = test.readdata('aima-alarm.xml')
+_data, _parents = xmlparser.readdata('aima-alarm.xml')
 _query = 'B'
 _evidences = ['j', 'm']
-# print(enumerate_ask(_query, get_variables(_query, _evidences, _data), _evidences, _data))
-# print(get_variables(_query, _evidences, _data))
-# for var in get_variables(_query, _evidences, _data):
-#     print(parent_es(var, _data))
-# for key in _data.keys():
-#     print(key)
-# print(related_vars('a', get_variables(_query, _evidences, _data), _data))
-elimination_ask(_query, ['A', 'E', _query], _evidences, _data, _parents)
+print(elimination_ask(_query, ['E', 'A', _query], _evidences, _data, _parents))
 # print(related_vars('A', ['A', 'E', 'B', 'j', 'm'], _data))
